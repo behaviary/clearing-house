6d5b525d7f147742170a33cfe9731d6d24388986
pndpo <peterdepaulo@gmail.com>
2015-12-06 20:52:44 -0800

    take out console.log

--- client/templates/front.js
+++ client/templates/front.js
@@ -2,7 +2,6 @@ Template.front.helpers({
   articles:function(){
 
   	var filteredArticles = _.uniq(Articles.find().fetch(), true, function(article) {
-  		console.log(article, article.root_id)
   		return article.root_id;
   	});
 
